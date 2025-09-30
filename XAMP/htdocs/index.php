a88f3fa0b230a387e8a21527e556f6de14965159
adamek121051 <adam.pelachy@gmail.com>
2025-09-30 10:55:56 +0200

    Add files via upload

---
+++ XAMP/htdocs/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo(SITE_NAME); ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+    <div class="header-logo">
+        <img src=avatars-000328000782-3wbizb-t1080x1080.jpg>
+    </div>
+</header>
+
+<h1>Vitaj na mojej stránke!</h1>
+
+<?php
+    $meno = "Klokotko";
+    echo "<p>Ahoj, $meno!</p>";
+?>
+
+<main>
+    <h1>nadpis</h1>
+    <p></p>
+</main>
+
+</body>
+</html>
